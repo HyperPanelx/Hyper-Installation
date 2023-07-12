a431b7732262e3a739bfde9d845327d88e36196e
Alireza Alikhani <alirezaalikhani277@gmail.com>
2023-07-12 09:30:19 +0000

    update

--- update.sh
+++ update.sh
@@ -12,6 +12,8 @@ services:
     environment:
       ENVPORT: 3838
       ENVMONGOPASS: mongo_password
+      ENVUSER: test
+      ENVPASS: P@ssw0rd
     networks:
         - hyper-installation_default
 networks:
@@ -21,8 +23,14 @@ networks:
 read -p "Enter Web Port: " web_port
 read -p "Enter SSH Port: "  ssh_port
 read -p "Enter DB Password: "  db_passwd
+read -p "Enter Your Panel Password: "  pass_panel
+read -p "Enter Your Panel Username: "  user_panel
+
 sudo sed -i "s/8081/$web_port/g" ./docker-compose.yml
 sudo sed -i "s/2222/$ssh_port/g" ./docker-compose.yml
 sudo sed -i "s/mongo_password/$db_passwd/g" ./docker-compose.yml
+sudo sed -i "s/test/$pass_panel/g" ./docker-compose.yml
+sudo sed -i "s/P@ssw0rd/$user_panel/g" ./docker-compose.yml
+
 sudo docker stop api
 sudo docker compose run -d
