540a05f29540d234c13265e4d01981b09041034b
Alireza Alikhani <62111040+officialalikhani@users.noreply.github.com>
2023-12-03 11:17:19 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,6 +43,8 @@ read -p "Enter SSH Port: "  ssh_port
 read -p "Enter DB Password: "  db_passwd
 htppassord=$(echo $(htpasswd -nb "$PAUSER" "$PAPASSWD") | sed -e s/\\$/\\$\\$/g)
 echo "$htppassord"
+# sudo sed -i "s/__htpasswd__/'$htppassord'/g" ./hyper-installation/docker-compose.yml
+sudo sed -i "s@__htpasswd__@'$htppassord'@g" ./hyper-installation/docker-compose.yml
 sudo sed -i "s/__htpasswd__/'$htppassord'/g" ./hyper-installation/docker-compose.yml
 sudo sed -i "s/_localhost_/'$domain_name'/g" ./hyper-front-vite/.env.production.local
 sudo sed -i "s/__email__/'$email_address'/g" ./hyper-installation/docker-compose.yml
