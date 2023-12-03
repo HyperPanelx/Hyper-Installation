7cea2b90d459437e2ee221bb0dd6ff08f0f1c70e
Alireza Alikhani <62111040+officialalikhani@users.noreply.github.com>
2023-12-03 11:06:02 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,6 +42,7 @@ read -p "Enter Web Port: " web_port
 read -p "Enter SSH Port: "  ssh_port
 read -p "Enter DB Password: "  db_passwd
 htppassord=$(echo $(htpasswd -nb "$PAUSER" "$PAPASSWD") | sed -e s/\\$/\\$\\$/g)
+echo $htppassord
 sudo sed -i "s/__htpasswd__/'$htppassord'/g" ./hyper-installation/docker-compose.yml
 sudo sed -i "s/_localhost_/'$domain_name'/g" ./hyper-front-vite/.env.production.local
 sudo sed -i "s/__email__/'$email_address'/g" ./hyper-installation/docker-compose.yml
@@ -63,4 +64,4 @@ echo -e "${GREEN}---------HyperPanel-------------${NC}"
 echo -e "URL: ${GREEN}https://hyper.$domain_name${NC}"
 echo -e "Username: ${GREEN}$PAUSER${NC}"
 echo -e "Password: ${GREEN}$PAPASSWD${NC}"
-echo -e ${GREEN}--------------------------------${NC}"
+echo -e "${GREEN}--------------------------------${NC}"
